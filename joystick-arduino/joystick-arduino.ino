080e93e802e70a041981d75faf2adee14766a84b
yishaya-ll <155594646+yishaya-ll@users.noreply.github.com>
2024-01-03 23:46:40 -0800

    Update joystick-arduino.ino

--- joystick-arduino/joystick-arduino.ino
+++ joystick-arduino/joystick-arduino.ino
@@ -1,9 +1,27 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int x;
+  int y;
+  int button;
 
+  x = analogRead(A0);
+  y = analogRead(A1);
+  button = digitalRead(7);
+
+  Serial.print("X: ");
+  Serial.print(x);
+
+  Serial.print("Y: ");
+  Serial.print(y);
+
+  Serial.print("B: ");
+  Serial.print(button);
+
+  Serial.println("");
+  delay(500);
 }
