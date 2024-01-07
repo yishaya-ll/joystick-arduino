ff68cef0c1eafa1eb16292d7261d891fe5bcddd8
yishaya-ll <155594646+yishaya-ll@users.noreply.github.com>
2024-01-07 03:24:10 -0800

    Update joystick-arduino.ino

--- joystick-arduino/joystick-arduino.ino
+++ joystick-arduino/joystick-arduino.ino
@@ -1,27 +1,64 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+int x=0;
+int y=0;
+int button=0;
+#define xAxis A0
+#define yAxis A1
+#define theButton 7
+
+
+
+void setup () {
+
+Serial.begin(9600);
+pinMode(theButton,INPUT_PULLUP);
+
+
+
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  int x;
-  int y;
-  int button;
 
-  x = analogRead(A0);
-  y = analogRead(A1);
-  button = digitalRead(7);
+x = analogRead(xAxis);
+y = analogRead(yAxis);
+button = digitalRead(theButton);
 
-  Serial.print("X: ");
-  Serial.print(x);
+/*
+Serial.print("X position");
+Serial.print(x);
+Serial.print("\T  ");
 
-  Serial.print("Y: ");
-  Serial.print(y);
 
-  Serial.print("B: ");
-  Serial.print(button);
+Serial.print("Y position");
+Serial.print(y);
+Serial.print("\T  ");
 
-  Serial.println("");
-  delay(500);
+Serial.print(button);
+Serial.println();
+*/
+
+
+
+if(x<490){
+  Serial.println("UP");
+}
+  else if(x>500){
+    Serial.println("DOWN");
+  }
+else {
+  Serial.println("X Centre");
+}
+if(y<500){
+  Serial.println("RIGHT");
+}
+  else if(y>510){
+    Serial.println("LEFT");
+  }
+else {
+  Serial.println("Y Centre");
+}
+Serial.println(button);
+delay(500);
+
+
 }
